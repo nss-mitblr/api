871449c977a82d8e90921a87e706c0654a9f7e9e
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2024-08-01 21:06:19 +0530

    Start on Custom Application Class

--- nss_api/app.py
+++ nss_api/app.py
@@ -1,3 +1,19 @@
 from sanic import Sanic
 
-appserver = Sanic("nss-api", strict_slashes=False)
+
+class NSS_API(Sanic):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.ctx.entra_public_keys = dict()
+
+    def get_entra_jwt_keys(self) -> dict:
+        return self.ctx.entra_public_keys
+
+    def set_entra_jwt_keys(self, keys: dict):
+        self.ctx.entra_public_keys = keys
+
+    def get_public_key(self) -> str:
+        return self.config["PRIV_KEY"]
+
+
+appserver = NSS_API("nss-api", strict_slashes=False)
