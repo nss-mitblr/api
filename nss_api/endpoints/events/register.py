4c8be211d01dfc8645e8c359417ee224c6860a9d
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2024-08-17 22:26:07 +0530

    Event Register endpoint

--- nss_api/endpoints/events/register.py
+++ nss_api/endpoints/events/register.py
@@ -1,5 +1,35 @@
+from sanic import Request, json
 from sanic.views import HTTPMethodView
+
+from nss_api.app import NSS_API
 
 
 class Event_Register(HTTPMethodView):
-    ...
+    async def post(self, request: Request):
+        """Register a student for an event."""
+        app: NSS_API = request.app
+        data = request.json
+        db_pool = app.get_db_pool()
+        user_id = app
+        async with db_pool.connection() as conn:
+            async with conn.cursor() as cur:
+                # Check if event exists
+                await cur.execute(
+                    "SELECT * FROM events WHERE event_id = %s", (data["event_id"],)
+                )
+                res = await cur.fetchone()
+                if not res:
+                    return json(
+                        {"success": False, "error": "Event does not exist."}, status=404
+                    )
+                # Register student for event.
+                await cur.execute(
+                    "INSERT INTO volunteer_hours_log (event_id, volunteer_id, hours, reason) VALUES (%s, %s, %s, %s)",  # noqa: E501
+                    (
+                        data["event_id"],
+                        user_id,
+                        0,
+                        "Registered for event.",
+                    ),
+                )
+        return json({"success": True})
