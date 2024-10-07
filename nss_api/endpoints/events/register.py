b0490e70ee0192dece31524d3b7ae255cfa11f40
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2024-10-07 22:50:42 +0530

    Updated event registeration login

--- nss_api/endpoints/events/register.py
+++ nss_api/endpoints/events/register.py
@@ -1,17 +1,20 @@
 from sanic import Request, json
 from sanic.views import HTTPMethodView
+from sanic_ext import validate
 
 from nss_api.app import NSS_API
+from nss_api.models.db.event import Event
 
 
 class Event_Register(HTTPMethodView):
+    @validate(json={"event_id": str})
     async def post(self, request: Request):
         """Register a student for an event."""
         app: NSS_API = request.app
         data = request.json
         db_pool = app.get_db_pool()
-        user_id = app
-        async with db_pool.connection() as conn:
+        user_id = app.decode_jwt(request.token)["email"]
+        async with db_pool.acquire() as conn:
             async with conn.cursor() as cur:
                 # Check if event exists
                 await cur.execute(
@@ -22,15 +25,25 @@ class Event_Register(HTTPMethodView):
                     return json(
                         {"success": False, "error": "Event does not exist."}, status=404
                     )
-                # Register student for event.
-                await cur.execute(
-                    "INSERT INTO Log (event_id, learner_id, hours, reason,type) VALUES (%s, %s, %s, %s, %s)",  # noqa: E501
-                    (
-                        data["event_id"],
-                        user_id,
-                        0,
-                        "Registered for event.",
-                        "event"
-                    ),
-                )
+                event = Event(res)
+                # Check if event is register-able
+                if event.is_closed():
+                    return json(
+                        {"success": False, "error": "Event is closed."}, status=400
+                    )
+                else:
+                    try:
+                        await cur.execute(
+                            "INSERT INTO Registerations (event_id, student_id) VALUES (%s, %s)",
+                            (data["event_id"], user_id),
+                        )
+                    except Exception:
+                        return json(
+                            {
+                                "success": False,
+                                "error": "You are already registered for this event.",
+                            },
+                            status=400,
+                        )
+
         return json({"success": True})
