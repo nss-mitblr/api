4a62ee90831417fa22ff1dd9a1aa392ec545fa1b
Alphaspiderman <68412330+Alphaspiderman@users.noreply.github.com>
2024-10-11 11:29:56 +0530

    Use the decorator injected JWTData in endpoints

--- nss_api/endpoints/me/about.py
+++ nss_api/endpoints/me/about.py
@@ -2,16 +2,18 @@ from sanic import Request, json
 from sanic.views import HTTPMethodView
 
 from nss_api.app import NSS_API
+from nss_api.decorators.require_login import require_login
 from nss_api.models.db.student import Student
+from nss_api.models.internal.jwt_data import JWT_Data
 
 
 class About_Me(HTTPMethodView):
-    async def get(self, request: Request):
+    @require_login()
+    async def get(self, request: Request, jwt_data: JWT_Data):
         app: NSS_API = request.app
         db_pool = app.get_db_pool()
-        # Get user ID from the token
-        data = app.decode_jwt(request.token)
-        user_id = data["email"]
+        # Get user ID
+        user_id = jwt_data.email
         # Get user data from the database
         async with db_pool.acquire() as conn:
             async with conn.cursor() as cur:
--- nss_api/endpoints/me/hour_log.py
+++ nss_api/endpoints/me/hour_log.py
@@ -2,16 +2,18 @@ from sanic import Request, json
 from sanic.views import HTTPMethodView
 
 from nss_api.app import NSS_API
+from nss_api.decorators.require_login import require_login
 from nss_api.models.db.volunteer_hours_log import Volunteer_Log
+from nss_api.models.internal.jwt_data import JWT_Data
 
 
 class My_Hour_Log(HTTPMethodView):
-    async def get(self, request: Request):
+    @require_login()
+    async def get(self, request: Request, jwt_data: JWT_Data):
         app: NSS_API = request.app
         db_pool = app.get_db_pool()
-        # Get user ID from the token
-        data = app.decode_jwt(request.token)
-        user_id = data["email"]
+        # Get user ID
+        user_id = jwt_data.email
         # Get user data from the database
         async with db_pool.acquire() as conn:
             async with conn.cursor() as cur:
@@ -23,13 +25,13 @@ class My_Hour_Log(HTTPMethodView):
                 logs = [Volunteer_Log(log) for log in data]
         return json({"data": [log.to_dict() for log in logs]})
 
-    async def post(self, request: Request):
+    @require_login()
+    async def post(self, request: Request, jwt_data: JWT_Data):
         """Triggers the server to update the user's hours on the profile."""
         app: NSS_API = request.app
         db_pool = app.get_db_pool()
-        # Get user ID from the token
-        data = app.decode_jwt(request.token)
-        user_id = data["email"]
+        # Get user ID
+        user_id = jwt_data.email
         # Get user data from the database
         async with db_pool.acquire() as conn:
             async with conn.cursor() as cur:
